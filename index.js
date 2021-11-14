9ca3cbb50027964dd3534adcb85957b267b74073
fttushar1997 <farhan.tanvir@tigerit.com>
2021-11-14 22:43:28 +0600

    middleware added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(function (req, res, next) {
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "X-Requested-With,Content-Type,X-CSRFToken"
   );
 
   // Set to true if you need the website to include cookies in the requests sent
