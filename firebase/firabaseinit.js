7a680518f3b3a9304996ebbb5f72498ec7f9e1fe
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2022-02-21 18:00:43 +0600

    Fix heroku private key parsing

--- firebase/firabaseinit.js
+++ firebase/firabaseinit.js
@@ -5,13 +5,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+var google_private_key = process.env.GOOGLE_PRIVATE_KEY;
+
 serviceAccount.type = process.env.GOOGLE_ACCOUNT_TYPE;
 serviceAccount.client_id = process.env.GOOGLE_CLIENT_ID;
-serviceAccount.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID.replace(
+serviceAccount.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID;
+serviceAccount.private_key = process.env.GOOGLE_PRIVATE_KEY.replace(
     /\\n/g,
     "\n"
 );
-serviceAccount.private_key = process.env.GOOGLE_PRIVATE_KEY;
 serviceAccount.client_email = process.env.GOOGLE_CLIENT_EMAIL;
 
 const admin = firebaseAdmin.initializeApp({
