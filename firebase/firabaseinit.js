2434f2b22f89ea8ee525011145ab79a8369d936a
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2022-02-21 17:04:14 +0600

    Remove console log in firebaseinit

--- firebase/firabaseinit.js
+++ firebase/firabaseinit.js
@@ -10,9 +10,6 @@ serviceAccount.client_id = process.env.GOOGLE_CLIENT_ID;
 serviceAccount.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID;
 serviceAccount.private_key = process.env.GOOGLE_PRIVATE_KEY;
 serviceAccount.client_email = process.env.GOOGLE_CLIENT_EMAIL;
-console.log("====================================");
-console.log(serviceAccount);
-console.log("====================================");
 
 const admin = firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
