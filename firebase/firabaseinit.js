19fd7bd76528dc62c864345cec401c3c85bb1b4e
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2022-02-21 16:46:25 +0600

    Fix secret keys issues

--- firebase/firabaseinit.js
+++ firebase/firabaseinit.js
@@ -1,9 +1,21 @@
 const firebaseAdmin = require("firebase-admin");
 
-const serviceAccount = require("./credentials.json");
+var serviceAccount = require("./credentials.json");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+serviceAccount.type = process.env.GOOGLE_ACCOUNT_TYPE;
+serviceAccount.client_id = process.env.GOOGLE_CLIENT_ID;
+serviceAccount.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID;
+serviceAccount.private_key = process.env.GOOGLE_PRIVATE_KEY;
+serviceAccount.client_email = process.env.GOOGLE_CLIENT_EMAIL;
+console.log("====================================");
+console.log(serviceAccount);
+console.log("====================================");
 
 const admin = firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
 });
 
 const storageRef = admin.storage().bucket(`gs://binni-catering.appspot.com`);
--- routes/auth.js
+++ routes/auth.js
@@ -79,106 +79,106 @@ const storageRef = require("../firebase/firabaseinit.js");
 //   }
 // });
 
-router.post("/register", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+router.post("/register", async(req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    var query_text =
-      "INSERT INTO users (first_name, last_name,mobile_number,email,password)\
+        var query_text =
+            "INSERT INTO users (first_name, last_name,mobile_number,email,password)\
       VALUES($1,$2,$3,$4,$5) RETURNING *;";
 
-    var values = [
-      req.body.first_name,
-      req.body.last_name,
-      req.body.mobile_number,
-      req.body.email,
-      hashedPassword,
-    ];
+        var values = [
+            req.body.first_name,
+            req.body.last_name,
+            req.body.mobile_number,
+            req.body.email,
+            hashedPassword,
+        ];
 
-    var newUser = await db.query(query_text, values);
-    newUser = newUser.rows[0];
-    delete newUser["password"];
+        var newUser = await db.query(query_text, values);
+        newUser = newUser.rows[0];
+        delete newUser["password"];
 
-    jwt.sign({ user: newUser }, process.env.SECRET_KEY, function (err, token) {
-      newUser.token = token;
-      res.status(201).json(newUser);
-    });
-  } catch (error) {
-    if (error.constraint) {
-      res.status(500).json({ error: error.constraint });
+        jwt.sign({ user: newUser }, process.env.SECRET_KEY, function(err, token) {
+            newUser.token = token;
+            res.status(201).json(newUser);
+        });
+    } catch (error) {
+        if (error.constraint) {
+            res.status(500).json({ error: error.constraint });
+        }
+        return res.status(500).json(error);
     }
-    return res.status(500).json(error);
-  }
 });
 
-router.post("/login", async (req, res) => {
-  try {
-    var query_text = "select *\
+router.post("/login", async(req, res) => {
+    try {
+        var query_text = "select *\
       from users \
       where email = $1;";
 
-    var values = [req.body.email];
+        var values = [req.body.email];
 
-    var user = await db.query(query_text, values);
+        var user = await db.query(query_text, values);
 
-    if (user.rowCount != 1) {
-      return res.status(404).json("User Not found");
+        if (user.rowCount != 1) {
+            return res.status(404).json("User Not found");
+        }
+
+        user = user.rows[0];
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!validPassword) {
+            return res.status(403).json({ error: "wrong password" });
+        }
+
+        delete user["password"];
+        jwt.sign({ user: user }, process.env.SECRET_KEY, function(err, token) {
+            user.token = token;
+            res.status(201).json(user);
+        });
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    user = user.rows[0];
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-
-    if (!validPassword) {
-      return res.status(403).json({ error: "wrong password" });
-    }
-
-    delete user["password"];
-    jwt.sign({ user: user }, process.env.SECRET_KEY, function (err, token) {
-      user.token = token;
-      res.status(201).json(user);
-    });
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 
-router.post("/admin/login", async (req, res) => {
-  try {
-    var query_text = "select *\
+router.post("/admin/login", async(req, res) => {
+    try {
+        var query_text = "select *\
       from admins \
       where username = ?;";
 
-    var values = [req.body.username];
+        var values = [req.body.username];
 
-    var admin = await runQuery(query_text, values);
+        var admin = await runQuery(query_text, values);
 
-    if (admin.length != 1) {
-      return res.status(404).json("Admin Not found");
+        if (admin.length != 1) {
+            return res.status(404).json("Admin Not found");
+        }
+
+        admin = admin[0];
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            admin.password
+        );
+
+        if (!validPassword) {
+            return res.status(403).json({ error: "wrong password" });
+        }
+
+        delete admin["password"];
+        jwt.sign({ admin: admin }, process.env.SECRET_KEY, function(err, token) {
+            admin.token = token;
+            res.status(201).json(admin);
+        });
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    admin = admin[0];
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      admin.password
-    );
-
-    if (!validPassword) {
-      return res.status(403).json({ error: "wrong password" });
-    }
-
-    delete admin["password"];
-    jwt.sign({ admin: admin }, process.env.SECRET_KEY, function (err, token) {
-      admin.token = token;
-      res.status(201).json(admin);
-    });
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 module.exports = router;
